c99e9db642a1db6542d3c7fd829faab62a9196c9
vishal patil <vishalpatil@gmail.com>
2023-02-02 15:06:23 +0530

    commit3 - mul

--- add.java
+++ add.java
@@ -6,7 +6,7 @@ public class add
 public static void main(String args[])   
 {  
 int n1 = 225, n2 = 115, x;  
-x = n1 - n2;  
+x = n1 * n2;  
 System.out.println("The sum of numbers is: "+x);  
 }  
 }  
