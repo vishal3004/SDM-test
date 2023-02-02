0a2f6e46813ef0e2ec986aa3e8c32d6e5461f79a
vishal patil <vishalpatil@gmail.com>
2023-02-02 14:54:15 +0530

    comit 1 - add

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.io.*;
+
+public class add  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, x;  
+x = n1 + n2;  
+System.out.println("The sum of numbers is: "+x);  
+}  
+}  
